a2ccaf1de062c29c751add723dd82adc3668ef80
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2018-02-05 11:52:52 +0100

    Add some tmux aliases

--- zsh/custom/10_aliases.zsh
+++ zsh/custom/10_aliases.zsh
@@ -1,3 +1,16 @@
+# -----------------------------------------------------------------------------
+# Nvim/Vim
+# -----------------------------------------------------------------------------
 alias e="nvim"
 alias vim="nvim"
+
+# -----------------------------------------------------------------------------
+# Tmux
+# -----------------------------------------------------------------------------
 alias tmux="TERM=tmux-256color tmux"
+alias tls="tmux ls"
+alias tns="tmux new-session -s"
+alias tnsd="tmux new-session -d -s"
+alias tnw="tmux new-window"
+alias tss="tmux switch-client -t"
+alias tas="tmux attach -t"
