ae6ade4e6dcd3324050af85ce9a802e7a0891efe
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2018-02-14 07:13:52 +0100

    Upgrade cloverage.

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[lein-ancient "0.6.14"]
-                  [lein-cloverage "1.0.9"]
+                  [lein-cloverage "1.0.10"]
                   [refactor-nrepl "2.3.1"]
                   [cider/cider-nrepl "0.16.0"]
                   [jonase/eastwood "0.2.5"]]
