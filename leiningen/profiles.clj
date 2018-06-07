cf89ca1f16099ed95569ca0c81bcdf8889f7fed4
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2018-06-07 07:37:14 +0200

    Fix profiles.clj

--- leiningen/profiles.clj
+++ leiningen/profiles.clj
@@ -1,6 +1,6 @@
-{:user {:plugins [[lein-ancient "0.6.14"]
+{:repl [[cider/cider-nrepl "0.17.0"]
+        [refactor-nrepl "2.3.1"]]
+ :user {:plugins [[lein-ancient "0.6.14"]
+                  [lein-cljfmt "0.5.7"]
                   [lein-cloverage "1.0.10"]
-                  [refactor-nrepl "2.3.1"]
-                  [cider/cider-nrepl "0.16.0"]
-                  [jonase/eastwood "0.2.5"]]
-        :dependencies [[cljfmt "0.5.7" :exclusions [org.clojure/clojure]]]}}
+                  [jonase/eastwood "0.2.6"]]}}
