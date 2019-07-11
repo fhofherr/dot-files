6da99975c0c440feea6f209796c4d73af0e6673b
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-07-11 20:20:47 +0200

    Add minimally required asdf plugins

--- asdf/install.zsh
+++ asdf/install.zsh
@@ -19,3 +19,17 @@ fi
 ASDF_VERSION="0.7.2"
 
 git_clone_or_pull https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch "v$ASDF_VERSION"
+
+if [ -d "$HOME/.asdf" ]; then
+   source "$HOME/.asdf/asdf.sh"
+   source "$HOME/.asdf/completions/asdf.bash"
+
+   asdf plugin-add python
+   asdf install python 3.7.4
+   asdf global python 3.7.4
+
+   asdf plugin-add nodejs
+   asdf install nodejs 12.6.0
+   asdf global nodejs 12.6.0
+fi
+
