dfc26a42ffad8eb1ba3f6a1548ef475c6305df5f
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-08-06 10:18:12 +0200

    Add bat and configure fzf for bat

--- zsh/custom/00_init.zsh
+++ zsh/custom/00_init.zsh
@@ -13,6 +13,15 @@ if [ -f "$HOME/.fzf.zsh" ]; then
    source "$HOME/.fzf.zsh"
 fi
 
+# Re-bind the fzf-file-widget from ^T to ^P. This makes it work just like
+# in my vim configuration.
+bindkey '^P' fzf-file-widget
+
+if command -v bat > /dev/null 2>&1
+then
+    export FZF_CTRL_T_OPTS="--preview 'bat --style=numbers,changes  --line-range=:15 --color always {} 2> /dev/null'"
+fi
+
 # -----------------------------------------------------------------------------
 # Go
 # -----------------------------------------------------------------------------
