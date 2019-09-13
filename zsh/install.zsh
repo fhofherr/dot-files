f62b032c52efd1f44d9b856268e52558fc150a22
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-09-13 17:16:28 +0200

    Use zplug instead of oh-my-zsh

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -10,23 +10,6 @@ else
 fi
 source "$DOTFILES_DIR/lib/functions.zsh"
 
-brew_install zsh
-apt_install zsh
-
-OH_MY_ZSH_REPO="https://github.com/robbyrussell/oh-my-zsh.git"
-git_clone_or_pull $OH_MY_ZSH_REPO $ZSH
-
 link_file "$DOTFILES_DIR/zsh/zshenv" "$HOME/.zshenv"
 link_file "$DOTFILES_DIR/zsh/zprofile" "$HOME/.zprofile"
 link_file "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
-
-if [ -z "$ZSH_CUSTOM" ]
-then
-    source "$HOME/.zshrc"
-fi
-
-DRACULA_ZSH_THEME="https://github.com/dracula/zsh.git"
-git_clone_or_pull $DRACULA_ZSH_THEME "$ZSH_CUSTOM/themes/dracula"
-git_clone_or_pull "https://github.com/zsh-users/zsh-autosuggestions" "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
-git_clone_or_pull "https://github.com/wfxr/forgit" "$ZSH_CUSTOM/plugins/forgit"
-link_file "$ZSH_CUSTOM/themes/dracula/dracula.zsh-theme" "$ZSH/themes/dracula.zsh-theme"
