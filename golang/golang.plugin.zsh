8e6163d5292d3e6d49d671001273215e4fd5a682
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-09-29 12:04:59 +0200

    Add _go_update_binaries function

--- golang/golang.plugin.zsh
+++ golang/golang.plugin.zsh
@@ -5,12 +5,35 @@ DOTFILES_GOLANG_PLUGIN_E_NOT_FOUND=254
 function _go_shim {
     local binname=$1
     shift
-    local binpath="$(go env GOPATH)/bin/$binname"
+    local binpath="$(command go env GOPATH)/bin/$binname"
     if [ ! -e  $binpath ]; then
         echo "not found: $binpath"
         return $DOTFILES_GOLANG_PLUGIN_E_NOT_FOUND
     fi
     $binpath $@
+}
+
+function _go_mod_get {
+    if [ $#@ = 0 ]; then
+        echo "Usage: _go_get [flags] <repo>"
+        return 1
+    fi
+    local curdir=$PWD
+    echo -n "Getting ${@[-1]}: "
+    if $(GO111MODULE=on command go get $@ > /dev/null 2>&1); then
+        echo "OK"
+    else
+        echo "Failed"
+    fi
+    cd $curdir
+}
+
+function _go_update_binaries {
+    _go_mod_get github.com/go-delve/delve/cmd/dlv@latest
+    _go_mod_get golang.org/x/tools/cmd/goimports@latest
+    _go_mod_get github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+    _go_mod_get golang.org/x/lint/golint@latest
+    _go_mod_get golang.org/x/tools/gopls@latest
 }
 
 function go-cov-pkg {
