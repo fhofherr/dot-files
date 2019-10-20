9985bca87290f18b041eb5dccca60e1dc86e58dd
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-10-20 09:47:55 +0200

    Autogenerate golang shims

--- golang/golang.plugin.zsh
+++ golang/golang.plugin.zsh
@@ -1,4 +1,12 @@
 # vim: set ft=zsh:
+if [ -f "$HOME/.zsh_dotfiles_init" ]
+then
+    source "$HOME/.zsh_dotfiles_init"
+else
+    echo "Could not find '$HOME/.zsh_dotfiles_init'!"
+    echo "Execute over-all install script!"
+fi
+
 
 DOTFILES_GOLANG_PLUGIN_E_NOT_FOUND=254
 
@@ -29,16 +37,37 @@ function _go_mod_get {
     cd $curdir
 }
 
+function _go_add_shim {
+    local binname="$1"
+    if [ -z "$binname" ]; then
+        echo "Usage: _go_add_shim <binary name>"
+        return 1
+    fi
+    mkdir -p "$DOTFILES_DIR/golang/shims"
+    local shimfile="$DOTFILES_DIR/golang/shims/$binname"
+    if [ -f "$shimfile" ]; then
+        return 0
+    fi
+    cat  > $shimfile <<EOF
+#!/usr/bin/env zsh
+
+source "\$DOTFILES_DIR/golang/golang.plugin.zsh"
+
+_go_shim $binname \$@
+EOF
+    chmod +x $shimfile
+}
+
 function _go_update_binaries {
-    _go_mod_get github.com/go-delve/delve/cmd/dlv@latest
-    _go_mod_get github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-    _go_mod_get github.com/psampaz/go-mod-outdated@latest
-    _go_mod_get golang.org/x/lint/golint@latest
-    _go_mod_get golang.org/x/tools/cmd/goimports@latest
-    _go_mod_get golang.org/x/tools/gopls@latest
+    _go_mod_get github.com/go-delve/delve/cmd/dlv@latest && _go_add_shim "dlv"
+    _go_mod_get github.com/golangci/golangci-lint/cmd/golangci-lint@latest && _go_add_shim "golangci-lint"
+    _go_mod_get github.com/psampaz/go-mod-outdated@latest && _go_add_shim "go-mod-outdated"
+    _go_mod_get golang.org/x/lint/golint@latest && _go_add_shim "golint"
+    _go_mod_get golang.org/x/tools/cmd/goimports@latest && _go_add_shim "goimports"
+    _go_mod_get golang.org/x/tools/gopls@latest && _go_add_shim "gopls"
 
     if command -v protoc > /dev/null 2>&1; then
-        _go_mod_get github.com/golang/protobuf/protoc-gen-go@latest
+        _go_mod_get github.com/golang/protobuf/protoc-gen-go@latest && _go_add_shim "protoc-gen-go"
     fi
 }
 
