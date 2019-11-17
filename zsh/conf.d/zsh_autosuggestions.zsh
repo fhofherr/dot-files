c67e7311b316518eff8ff3fab45fc0ca18d67b23
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2019-11-17 11:52:43 +0100

    Select autosuggestions color if required

--- zsh/conf.d/zsh_autosuggestions.zsh
+++ zsh/conf.d/zsh_autosuggestions.zsh
@@ -2,10 +2,14 @@
 # zsh-autosuggestions
 # -----------------------------------------------------------------------------
 
-# zsh-autosuggestions in konsole with the dracula theme are not displayed
-# because the default foreground color is more or less the same as the
-# background color. Therefore we set it to a nicer color.
-#
-# The number was determined by runing spectrum_ls and selecting a color we
-# like.
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=239"
+case "$DOTFILES_COLOR_THEME" in
+    "dracula")
+        # zsh-autosuggestions in konsole with the dracula theme are not
+        # displayed because the default foreground color is more or less the
+        # same as the background color. Therefore we set it to a nicer color.
+        #
+        # The number was determined by runing spectrum_ls and selecting a color
+        # we like.
+        ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=239"
+        ;;
+esac
