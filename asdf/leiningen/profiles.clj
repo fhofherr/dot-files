d129e91c5b878b7d94b60fb8ccc4df8af74319c0
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2020-01-04 17:03:50 +0100

    Remove clojure config

--- asdf/leiningen/profiles.clj
+++
@@ -1,6 +0,0 @@
-{:repl [[cider/cider-nrepl "0.17.0"]
-        [refactor-nrepl "2.3.1"]]
- :user {:plugins [[lein-ancient "0.6.14"]
-                  [lein-cljfmt "0.5.7"]
-                  [lein-cloverage "1.0.10"]
-                  [jonase/eastwood "0.2.6"]]}}
