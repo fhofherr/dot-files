0eda4223f53006cc9bbeb44b38bf4b8740b30c60
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2020-02-06 16:55:00 +0100

    Add pre-commit to git plugin

--- git/git.plugin.zsh
+++ git/git.plugin.zsh
@@ -3,6 +3,11 @@
 : ${DOTFILES_DIR:=$HOME/dot-files}
 : ${DOTFILES_BIN_DIR:=$DOTFILES_DIR/bin}
 
+: ${DOTFILES_GIT_PRE_COMMIT_ENABLED:=true}
+: ${DOTFILES_GIT_PRE_COMMIT_VENV:=$HOME/.local/dotfiles/pre_commit_venv}
+: ${DOTFILES_GIT_PRE_COMMIT_BIN:=$DOTFILES_GIT_PRE_COMMIT_VENV/bin}
+: ${DOTFILES_GIT_PRE_COMMIT_PIP:=$DOTFILES_GIT_PRE_COMMIT_BIN/pip}
+
 if ! command -v git > /dev/null 2>&1; then
     echo "Git is not installed"
     return 1
@@ -14,6 +19,14 @@ function _dotfiles_configure_git {
     command git config --global include.path "$DOTFILES_DIR/git/gitconfig.common"
 }
 
+function _dotfiles_git_install_pre_commit {
+    command mkdir -p "$(dirname $DOTFILES_GIT_PRE_COMMIT_VENV)"
+    command python3 -m venv "$DOTFILES_GIT_PRE_COMMIT_VENV"
+
+    "$DOTFILES_GIT_PRE_COMMIT_PIP" install --no-user --upgrade pip
+    "$DOTFILES_GIT_PRE_COMMIT_PIP" install --no-user --upgrade -r "$DOTFILES_DIR/git/requirements.txt"
+}
+
 if [ ! -e "$HOME/.gitignore_global" ]
 then
     printf "Configure git plugin? [y/N]: "
@@ -21,6 +34,16 @@ then
         echo
         _dotfiles_configure_git
     fi
+fi
+
+if $DOTFILES_GIT_PRE_COMMIT_ENABLED && [ ! -d "$DOTFILES_GIT_PRE_COMMIT_VENV" ]
+then
+    printf "Install pre-commit? [y/N]: "
+    if read -q; then
+        echo
+        _dotfiles_git_install_pre_commit
+    fi
+    export PATH="$DOTFILES_GIT_PRE_COMMIT_BIN:$PATH"
 fi
 
 export PATH="$DOTFILES_DIR/git/bin:$PATH"
