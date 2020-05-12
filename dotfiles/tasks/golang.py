ee16c50b3e369c94888a37e93eb442c7c7f9307b
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2020-05-12 08:55:03 +0200

    Add go-addtags plugin

--- dotfiles/tasks/golang.py
+++ dotfiles/tasks/golang.py
@@ -21,7 +21,8 @@ OFFICIAL_DEV_TOOLS = [
 THIRD_PARTY_DEV_TOOLS = [
     "github.com/go-delve/delve/cmd/dlv@latest",
     "github.com/aarzilli/gdlv@latest",
-    "github.com/psampaz/go-mod-outdated@latest"
+    "github.com/psampaz/go-mod-outdated@latest",
+    "github.com/fatih/gomodifytags@latest"
 ]
 
 
