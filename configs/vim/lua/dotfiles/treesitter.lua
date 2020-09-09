3655ee8d3662c909a398502aa69334d9910fe837
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2020-09-09 22:24:21 +0200

    Play with nvim-treesitter

--- configs/vim/lua/dotfiles/treesitter.lua
+++ configs/vim/lua/dotfiles/treesitter.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local has_treesitter, treesitter = pcall(require, "nvim-treesitter")
-local has_treesitter_configs, treesitter_configs = pcall(require, "nvim-treesitter/configs")
+local has_treesitter_configs, treesitter_configs = pcall(require, "nvim-treesitter.configs")
 
 function M.setup()
     if not has_treesitter_configs then
@@ -9,17 +9,42 @@ function M.setup()
     end
 
     treesitter_configs.setup {
-        highlight = {
-            enable = true
+        highlight = { enable = true },
+        incremental_selection = { enable = true },
+        refactor = {
+            highlight_definitions = { enable = true },
+            smart_rename = {
+                enable = true,
+                disable = { "go", "python" }, -- We use the language server for that
+                keymaps = {
+                    smart_rename = "<F2>"
+                },
+            },
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                disable = { "go" }, -- Gopher.vim defines textobjects for us
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@comment.outer", -- Same as defined by gopher.vim for go
+                    ["ic"] = "@comment.inner", -- Same as defined by gopher.vim for go
+                },
+            },
         },
         ensure_installed = {
             "bash",
             "c",
             "go",
+            "html",
             "json",
             "lua",
+            "markdown",
             "python",
-            "toml"
+            "regex",
+            "toml",
+            "yaml"
         }
     }
 end
