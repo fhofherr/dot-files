f2dc429207ca9cd2dc8ba4328b89d023163324a2
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-03-17 21:34:47 +0100

    Add mnml module

---
+++ modules/mnml/mod.py
@@ -0,0 +1,21 @@
+import os
+
+from dotfiles import module
+
+MNML_PKG = "github.com/fhofherr/mnml/cmd/mnml"
+
+
+class MNML(module.Definition):
+    required = ["golang"]
+
+    def _mnml_cmd(self):
+        return os.path.join(self.golang.go_bin_dir, "mnml")
+
+    @module.update
+    @module.install
+    def install(self):
+        self.golang.go_get(MNML_PKG)
+
+
+if __name__ == "__main__":
+    module.run(MNML)
