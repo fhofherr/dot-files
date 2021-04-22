1b0a30263865e2a163186694e1bb32f178dbd6f4
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-04-22 10:30:02 +0200

    Configure build gopls build flags

--- modules/neovim/lua/dotfiles/settings/lsp/gopls.lua
+++ modules/neovim/lua/dotfiles/settings/lsp/gopls.lua
@@ -4,6 +4,18 @@ local plugin = require("dotfiles.plugin")
 local lspconfig = plugin.safe_require("lspconfig")
 local defaults = require("dotfiles.settings.lsp.defaults")
 
+local function build_flags()
+    local flags = {}
+
+    local tags = vim.env.GOPLS_BUILD_TAGS
+    if tags and tags ~= "" then
+        flags[#flags+1] = "-tags"
+        flags[#flags+1] = tags
+    end
+
+    return flags
+end
+
 function M.setup()
     local gopls_opts = defaults.new_defaults()
 
@@ -11,6 +23,7 @@ function M.setup()
     gopls_opts.init_options = {
         usePlaceholders = true,
         completeUnimported = true,
+        buildFlags = build_flags(),
     }
     gopls_opts.capabilities.textDocument.completion.completionItem.snippetSupport = true
 
