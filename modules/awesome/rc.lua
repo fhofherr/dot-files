289bec61e7d48cd9f698dab54dae007d0d1394e3
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-04-26 21:53:48 +0200

    Disable titlebars

--- modules/awesome/rc.lua
+++ modules/awesome/rc.lua
@@ -195,10 +195,10 @@ awful.rules.rules = {
             },
         },
         properties = { floating = true },
-    }, -- Add titlebars to normal clients and dialogs
+    }, -- Remove titlebars to normal clients and dialogs
     {
         rule_any = { type = { "normal", "dialog" } },
-        properties = { titlebars_enabled = true },
+        properties = { titlebars_enabled = false },
     },
 
     -- Set Firefox to always map on the tag named "2" on screen 1.
