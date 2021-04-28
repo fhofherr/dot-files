5269f0826a142a02e0cacc47bc054a12a4e45af5
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-04-28 19:15:35 +0200

    Always add tile.right layout

--- modules/awesome/dotfiles/awesome/layout.lua
+++ modules/awesome/dotfiles/awesome/layout.lua
@@ -5,17 +5,17 @@ require("awful.autofocus")
 
 local _, lain = pcall(require, "lain")
 
-local big_screen = 1
-local small_screen = 2
-
 local layouts = {}
+local small_screen = 1
+local big_screen = 2
 
+layouts[small_screen] = awful.layout.suit.max
 if lain then
     layouts[big_screen] = lain.layout.centerwork
+    layouts[#layouts + 1] = awful.layout.suit.tile.right
 else
     layouts[big_screen] = awful.layout.suit.tile.right
 end
-layouts[small_screen] = awful.layout.suit.max
 layouts[#layouts + 1] = awful.layout.suit.floating
 
 function M.for_screen(screen)
