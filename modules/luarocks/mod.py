ff6aab01037f944d1b5d30f84f9fd6710d56f762
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-05-06 19:28:35 +0200

    Fix path to luarocks binaries

--- modules/luarocks/mod.py
+++ modules/luarocks/mod.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 
 from dotfiles import module
@@ -12,8 +13,12 @@ class Luarocks(module.Definition):
     def _after_compinit_script(self):
         if not self._luarocks_cmd:
             return ""
-        p = self("path", "--bin", capture_output=True)
+        p = self("path", "--no-bin", capture_output=True)
         return p.stdout.decode("utf-8").strip()
+
+    @property
+    def _luarocks_bin(self):
+        return os.path.join(self.home_dir, ".luarocks", "bin")
 
     @module.install
     @module.update
@@ -21,6 +26,7 @@ class Luarocks(module.Definition):
         if not shutil.which("luarocks"):
             self.log.info("Luarocks not found. Skipping luarocks module")
             return
+        self.state.setenv("PATH", self._luarocks_bin)
         self.state.zsh.after_compinit_script = self._after_compinit_script
 
     def __call__(self, *args, **kwargs):
