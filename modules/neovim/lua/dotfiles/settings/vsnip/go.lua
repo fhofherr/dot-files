512cf2e2c330bcb8a56df0f36d5e93f3dbd69213
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-05-10 14:38:15 +0200

    Fix Golang pkg snippet

--- modules/neovim/lua/dotfiles/settings/vsnip/go.lua
+++ modules/neovim/lua/dotfiles/settings/vsnip/go.lua
@@ -4,7 +4,7 @@ function M.pkg_name()
     local file_name = vim.fn.expand("%:t:r")
     local dir_name = vim.fn.expand("%:h:t")
 
-    if vim.endswith(file_name, "_test") and not vim.endswith("file_name", "_internal_test") then
+    if vim.endswith(file_name, "_test") and not vim.endswith(file_name, "_internal_test") then
         return dir_name .. "_test"
     end
     return dir_name
