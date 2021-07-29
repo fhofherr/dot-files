2cd3bb4981cdf2f3f960692ef71db0dace3b62b2
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-07-29 08:03:40 +0200

    Configure python formatters

--- modules/neovim/lua/dotfiles/plugin/formatter.lua
+++ modules/neovim/lua/dotfiles/plugin/formatter.lua
@@ -8,8 +8,42 @@ local function lua_format()
     return {exe = plugin.hererocks_bin() .. "/lua-format", stdin = true}
 end
 
+local function python_isort()
+    return {exe = "isort", args = {"-rc"}, stdin = false}
+end
+
+local function python_yapf_or_black()
+    if vim.fn.executable("black") == 1 then
+        return {exe = "black", args = {"-"}, stdin = true}
+    else
+        return {exe = "yapf", stdin = true}
+    end
+end
+
+local function config_python()
+    local cfg = {}
+
+    if vim.fn.executable("isort") == 1 then
+        cfg[#cfg + 1] = python_isort
+    end
+    if vim.fn.executable("black") == 1 or vim.fn.executable("yapf") == 1 then
+        cfg[#cfg + 1] = python_yapf_or_black
+    end
+
+    if #cfg == 0 then
+        return nil
+    end
+    return cfg
+end
+
 function M.config()
-    formatter.setup({logging = false, filetype = {lua = {lua_format}}})
+    formatter.setup({
+        logging = false,
+        filetype = {
+            lua = {lua_format},
+            python = config_python(),
+        },
+    })
 end
 
 return M
