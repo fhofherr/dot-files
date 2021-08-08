93064fb49a430dfe741268346af47dd47146531e
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-08-08 16:25:11 +0200

    Add clang-format to formatter.nvim

--- modules/neovim/lua/dotfiles/plugin/formatter.lua
+++ modules/neovim/lua/dotfiles/plugin/formatter.lua
@@ -3,6 +3,20 @@ local M = {}
 local formatter = require("formatter")
 local plugin = require("dotfiles.plugin")
 
+local function config_clang()
+    return {
+        -- clang-format
+        function()
+            return {
+                exe = "clang-format",
+                args = {"--assume-filename", vim.api.nvim_buf_get_name(0)},
+                stdin = true,
+                cwd = vim.fn.expand('%:p:h') -- Run clang-format in cwd of the file.
+            }
+        end
+    }
+end
+
 -- lua-fmt https://github.com/Koihik/LuaFormatter
 local function lua_format()
     return {exe = plugin.hererocks_bin() .. "/lua-format", stdin = true}
@@ -40,6 +54,8 @@ function M.config()
     formatter.setup({
         logging = false,
         filetype = {
+            c = config_clang(),
+            cpp = config_clang(),
             lua = {lua_format},
             python = config_python(),
         },
