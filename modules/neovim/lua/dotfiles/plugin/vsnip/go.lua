ddf5430c09d198d5a1e054a171fa0d5950da392c
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-10-04 15:44:37 +0200

    Fix package name generation

--- modules/neovim/lua/dotfiles/plugin/vsnip/go.lua
+++ modules/neovim/lua/dotfiles/plugin/vsnip/go.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.pkg_name()
     local file_name = vim.fn.expand("%:t:r")
-    local dir_name = vim.fn.expand("%:h:t")
+    local dir_name = vim.fn.expand("%:p:h:t")
 
     if vim.endswith(file_name, "_test") and not vim.endswith(file_name, "_internal_test") then
         return dir_name .. "_test"
