08b8d981d7fab32503fa2a408022ad0febc6f108
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-10-27 09:39:24 +0200

    Re-enable vimade

--- modules/neovim/lua/dotfiles/plugin/init.lua
+++ modules/neovim/lua/dotfiles/plugin/init.lua
@@ -127,12 +127,12 @@ function M.setup()
 			})
 
 			use("unblevable/quick-scope")
-			-- use {
-			--     "TaDaa/vimade",
-			--     config = function()
-			--         require("dotfiles.plugin.vimade").config()
-			--     end
-			-- }
+			use {
+			    "TaDaa/vimade",
+			    config = function()
+			        require("dotfiles.plugin.vimade").config()
+			    end
+			}
 
 			use({
 				"mhinz/vim-startify",
