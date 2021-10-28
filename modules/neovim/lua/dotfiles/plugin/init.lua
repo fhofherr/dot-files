2cdb466eaa969e2fecbcabf4fa9a710e3bcdc4bc
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-10-28 19:45:20 +0200

    Fix startify config

--- modules/neovim/lua/dotfiles/plugin/init.lua
+++ modules/neovim/lua/dotfiles/plugin/init.lua
@@ -138,7 +138,8 @@ function M.setup()
 				"mhinz/vim-startify",
 				config = function()
 					vim.g.startify_session_persistence = 1
-					vim.g.starify_change_to_vcs_root = 1
+					vim.g.startify_change_to_dir = 0
+					vim.g.startify_change_vcs_root = 1
 				end,
 			})
 			use("mhinz/vim-signify")
