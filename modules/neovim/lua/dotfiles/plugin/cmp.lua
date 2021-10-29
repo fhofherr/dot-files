804e1367323e83487ddc40f2fe4129e7fd02b47b
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2021-10-29 17:00:13 +0200

    Update nvim-cmp config

--- modules/neovim/lua/dotfiles/plugin/cmp.lua
+++ modules/neovim/lua/dotfiles/plugin/cmp.lua
@@ -4,38 +4,36 @@ local cmp = require("cmp")
 local npairs_cmp = require("nvim-autopairs.completion.cmp")
 
 function M.config()
-    cmp.setup({
-        completion = {
-            completeopt = "menu,menuone,noselect,noinsert",
-        },
-        preselect = cmp.PreselectMode.None,
-        snippet = {
-            expand = function(args)
-                vim.fn["vsnip#anonymous"](args.body)
-            end,
-        },
-        sources = {
-            { name = "nvim_lsp" },
-            { name = "nvim_lua" },
-            { name = "buffer" },
-            { name = "path" },
-            { name = "vsnip" },
-            { name = "emoji" },
-        },
-        mapping = {
-            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<C-e>"] = cmp.mapping.close(),
-            ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-        },
-    })
-    npairs_cmp.setup({
-        map_cr = true,
-        insert = true,
-        map_complete = true,
-        auto_select = false,
-    })
+	cmp.setup({
+		completion = {
+			completeopt = "menu,menuone,noselect,noinsert",
+		},
+		preselect = cmp.PreselectMode.None,
+		snippet = {
+			expand = function(args)
+				vim.fn["vsnip#anonymous"](args.body)
+			end,
+		},
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "nvim_lua" },
+			{ name = "buffer" },
+			{ name = "path" },
+			{ name = "vsnip" },
+			{ name = "emoji" },
+		},
+		mapping = {
+			["<C-d>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<C-e>"] = cmp.mapping.close(),
+			["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+		},
+	})
+
+	-- See https://github.com/windwp/nvim-autopairs/issues/171 for info
+	cmp.event:on("confirm_done", npairs_cmp.on_confirm_done())
 end
 
 return M
