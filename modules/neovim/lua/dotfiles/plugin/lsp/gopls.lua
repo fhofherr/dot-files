d451afd9dedc66acf526d2c5474384b02653adf8
Ferdinand Hofherr <mail@ferdinandhofherr.de>
2022-05-09 15:23:44 +0200

    Update gopls config

--- modules/neovim/lua/dotfiles/plugin/lsp/gopls.lua
+++ modules/neovim/lua/dotfiles/plugin/lsp/gopls.lua
@@ -2,6 +2,7 @@ local M = {}
 
 local plugin = require("dotfiles.plugin")
 local lspconfig = plugin.safe_require("lspconfig")
+local util = plugin.safe_require("lspconfig.util")
 
 local function build_flags()
 	local flags = {}
@@ -17,13 +18,32 @@ end
 
 function M.setup(opts)
 	opts.cmd = {
-		"gopls", --[["--remote=auto"]]
+		"gopls",
+		"serve",
+		-- "--remote=auto"
 	}
-	opts.init_options = {
-		usePlaceholders = true,
-		completeUnimported = true,
-		buildFlags = build_flags(),
+	opts.root_dir = util.root_pattern("go.work", "go.mod", ".git")
+	-- See https://github.com/golang/tools/blob/master/gopls/doc/settings.md
+	opts.settings = {
+		gopls = {
+			usePlaceholders = true,
+			buildFlags = build_flags(),
+			analyses = {
+				unreachable = true,
+				unusedparams = true,
+			},
+			codelenses = {
+				generate = true,
+				gc_details = true,
+				regenerate_cgo = true,
+				test = true,
+				tidy = true,
+				upgrade_dependency = true,
+				vendor = false,
+			},
+		},
 	}
+
 	opts.capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 	lspconfig.gopls.setup(opts)
